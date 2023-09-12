d18b4d0763f6c1adcdcc82691abca918d3775c2b
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2023-09-12 11:14:13 -0300

    primeiros passos php part tres

--- aula1/index.php
+++ aula1/index.php
@@ -1,13 +1,42 @@
 <?php
 
-$heart = 'coisa'; 
-$heart45 = 'alguma'; 
-$thear = '12'; 
-$HeARt = 'coisa'; 
+$a = 10;
+$b = 20;
+$soma = $a + $b;
+echo $soma . "\n";
 
-echo $heart45 . $heart . $thear . $HeARt;
+$a = 90;
+$b = 20;
+$subtracao = $a - $b;
+echo $subtracao . "\n";
 
-$idade = '20 anos';
-echo " Sou Vini e tenho " . $idade . "\n";
+$a = 10;
+$b = 200;
+$mult = $a * $b;
+echo $mult . "\n";
+
+$a = 5;
+$b = 20;
+$div = $a / $b;
+echo $div . "\n";
+
+$a = 100;
+$b = 459;
+$resto = $a % $b;
+echo $resto . "\n";
+
+$a = 100;
+$b = 459;
+$expo = $a ** $b;
+echo $expo . "\n";
+
+// ou as formulas podem ser executadas desta formas
+
+echo (50 + 10 - 30) * (5.2 / 2);
+
+// ou
+
+$a = 2;
+echo $a * ($b = 3);
 
 ?>
