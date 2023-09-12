1a9d2deeee52e464e5502cc1462ce747dece819c
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2023-09-12 10:41:13 -0300

    primeiros passos php part dois

--- aula1/index.php
+++ aula1/index.php
@@ -1,7 +1,44 @@
 <?php
+// booleans / bool
 
-var_dump("olá");
-var_dump(123);
-var_dump(new stdClass());
+var_dump(true);
+var_dump(false);
+
+// inteiros
+
+var_dump(123);   // Numero decimal
+var_dump(-123);  // Numeros negativos
+var_dump(0123);  // numero octal
+var_dump(0x1A);  // numero hexagonal
+var_dump(0b111); // numero binario
+
+// numeros flutuantes
+
+$a = 1.234;
+$b = 1.2e3;
+$c = 7E-10;
+
+echo $a . "\n";
+echo $b . "\n";
+echo $c . "\n";
+
+// strings
+
+echo "opa opa opa \n";
+echo "aaaaaaaaaaaaaaaaa";
+
+// arrays (vetores)
+
+$array1 = array(
+    "dev" => "vini",
+    "gp" => "php"
+);
+
+// ou
+
+$array2 = [
+    "dev" => "vini",
+    "gp" => "php"
+];
 
 ?>
