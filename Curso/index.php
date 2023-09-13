750afce88111c3242d645d8c7cbb123ac24e76ed
Vini <rossinicostavinicius@gmail.com>
2023-09-13 16:26:28 -0300

    interfaces

--- Curso/index.php
+++ Curso/index.php
@@ -38,4 +38,120 @@ class Saudar implements helloWorld {
 $cumprimentos = new Saudar();
 $cumprimentos->helloWorld();
 
+// implementacao com 2 interfaces
+
+  // criamos a interface Nome e definimos os métodos para ela:
+  interface Nome 
+  {
+      public function setNome($nome);
+      public function getNome();
+  }
+
+// criamos a interface Idade e definimos seu métodos:
+  interface Idade 
+  {
+      public function setIdade($idade);
+      public function getIdade();
+  }
+
+// criamos a classe Pessoa e definimos que ela deverá conter os métodos definidos nas interfaces Nome e Idade:
+  class Pessoa implements Nome, Idade 
+  {
+      private $nome;
+      private $idade;
+      
+      public function getNome()
+      {
+          return $this->nome;
+      }
+
+      public function setNome($nome)
+      {
+          $this->nome = $nome;
+      }
+
+      public function getIdade()
+      {
+          return $this->idade;
+      }
+
+      public function setIdade($idade)
+      {
+          $this->idade = $idade;
+      }
+
+      public static function apresentarPessoa($pessoa)
+      {
+          echo "Olá, eu me chamo {$pessoa->getNome()} e tenho {$pessoa->getIdade()} anos.";
+      }
+  }
+
+// instanciamos um novo objeto através da classe Pessoa:
+  $novaPessoa = new Pessoa();
+// atribuimos valores aos seus atributos com os setters:
+  $novaPessoa->setNome('João');
+  $novaPessoa->setIdade(36);
+// acessamos e executamos o método estático para apresentação de pessoas:
+  Pessoa::apresentarPessoa($novaPessoa);
+
+// interface estendiveis
+
+  // definimos a interface CalculosBasicos e seus métodos:
+  interface CalculosBasicos
+  {
+      public function somar($n1, $n2);
+      public function subtrair($n1, $n2);
+  }
+
+// definimos a interface Matematica, seu métodos e que ela será um interface estendida de CalculosBasicos,
+// a partir de agora a classe que implementar Matematica deverá possuir tanto métodos definidos na própria
+// interface, quanto métodos definidos em Calculos Basicos;
+  interface Matematica extends CalculosBasicos
+  {
+      public function calcularHipotenusa($cateto1, $cateto2);
+  }
+
+// definimos uma classe Matematico que implementa a interface estendível Matematica:
+  class Matematico implements Matematica
+  {
+      public function somar($n1, $n2)
+      {
+          return $n1 + $n2;
+      }
+
+      public function subtrair($n1, $n2)
+      {
+          return $n1 - $n2;
+      }
+
+      public function calcularHipotenusa($cateto1, $cateto2)
+      {
+        // calculamos a hipotenusa e devolvemos seu valor
+          $hipotenusa = ($cateto1**2 + $cateto2**2)**0.5;
+          return $hipotenusa;
+      }
+  }
+
+// instanciamos um objeto e acessamos seu atributos
+  $professor = new Matematico();
+  echo $professor->somar(2,4)."-"; // 6
+  echo $professor->subtrair(12, 10)."-"; // 2
+// usamos o método number_format para mostrar apenas 2 casas decimais do resultado
+  echo number_format($professor->calcularHipotenusa(12, 15), 2); // 19.21
+
+// usando as interfaces
+interface Exemplo 
+{
+  const testando = 'Valor em Exemplo';
+}
+
+echo Exemplo::testando.' - ';
+
+class ClasseTeste implements Exemplo
+{
+  // ...
+}
+
+echo ClasseTeste::testando;
+
 ?>
