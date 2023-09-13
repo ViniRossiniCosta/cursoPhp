8fc8039e622e07f95f54721a250a9e137dda6d1f
Vini <rossinicostavinicius@gmail.com>
2023-09-13 16:23:42 -0300

    interfaces

--- Curso/index.php
+++ Curso/index.php
@@ -1,23 +1,41 @@
 <?php
-// setters
+// interfaces
 /**
- * Setters são métodos usados para alterar valor de propriedades 
- * que não podem ser alteradas diretamente.
+ * Bom, para entendermos interfaces, vamos pegar uma definição da própria página do PHP:
+ * Interfaces de objetos permitem a criação de códigos que especificam quais métodos uma classe 
+ * deve implementar, sem definir como esses métodos serão tratados.
+ * Mas então, o que isso significa?
+ * Significa que interfaces são nada mais, 
+ * nada menos do que modelos de métodos que podemos implementar em uma classe 
+ * (ou até mesmo estende-lá em outra interface como veremos mais a seguir), 
+ * e que ao fazer essa implementação a classe passa a ser obrigada à conter 
+ * os métodos declarados na interface, resultando em um erro caso não seja feito;
+ * Em outras palavras, podemos entender interfaces como um contrato, 
+ * a partir do momento que você assina esse contrato (implementa ele),
+ * você passa a ser obrigado a seguir as especificações previstas nele, 
+ * o descumprimento delas seria punitivo (que no nosso caso seria um erro fatal do código).
  */
-// nao estatico 
-class Exemplo {
-  private $propPrivate = 'esta propriedade não pode ser alterada diretamente';
-  public function setterPropPrivate($novoValor){
-    $this->propPrivate = $novoValor;
+
+interface provedorPagamento {
+  // declaracao de metodos
+ }
+
+interface paypal implements provedorPagamento{
+  // add metodos e atributos da classe
+}
+
+// implementação certa de uma interface
+interface HelloWorld {
+  public function helloWorld():void
+}
+
+class Saudar implements helloWorld {
+  public function helloWorld() {
+    echo 'ola mundo !';
   }
 }
 
-// estatico
-class Exemplo {
-    private static $propPrivate = "Esta propriedade não pode ser alterada ou lida diretamente";
-    public static function setterPropPrivate(string $novoValor) {
-        self::$propPrivate = $novoValor;
-    }
-}
+$cumprimentos = new Saudar();
+$cumprimentos->helloWorld();
 
 ?>
