167b100ff7eb52eade4676ca94707a2ffb5f9195
Vini <rossinicostavinicius@gmail.com>
2023-09-13 16:04:49 -0300

    setters

---
+++ Curso/index.php
@@ -0,0 +1,23 @@
+<?php
+// setters
+/**
+ * Setters são métodos usados para alterar valor de propriedades 
+ * que não podem ser alteradas diretamente.
+ */
+// nao estatico 
+class Exemplo {
+  private $propPrivate = 'esta propriedade não pode ser alterada diretamente';
+  public function setterPropPrivate($novoValor){
+    $this->propPrivate = $novoValor;
+  }
+}
+
+// estatico
+class Exemplo {
+    private static $propPrivate = "Esta propriedade não pode ser alterada ou lida diretamente";
+    public static function setterPropPrivate(string $novoValor) {
+        self::$propPrivate = $novoValor;
+    }
+}
+
+?>
